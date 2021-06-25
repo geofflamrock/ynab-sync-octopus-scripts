37fd77133c881e4063bc5d34b5609acf7ef9a76a
Geoff Lamrock <geoff.lamrock@gmail.com>
2021-06-25 14:56:29 +1000

    Changes to use cli (#1)

--- src/westpac-au/index.ts
+++ src/westpac-au/index.ts
@@ -1,71 +1,68 @@
-import { format, subDays } from "date-fns";
+import { format, isValid, parseISO, subDays } from "date-fns";
 import {
   ITransactionImporter,
   ITransactionParser,
   OfxTransactionParser,
   YnabTransactionImporter,
 } from "ynab-sync-core";
-import {
-  WestpacTransactionExporter,
-  WestpacTransactionExportInputs,
-} from "ynab-sync-westpac-au";
+import { WestpacTransactionExporter } from "ynab-sync-westpac-au";
+import commander from "commander";
 
-const exportTransactions = async () => {
-  const numberOfDaysToSync = parseInt(
-    process.env["Sync.Westpac.NumberOfDays"] ||
-      "#{Sync.Westpac.NumberOfDays}" ||
-      "7"
-  );
+export type WestpacTransactionExportParams = {
+  westpacUsername: string;
+  westpacPassword: string;
+  westpacAccountName: string;
+  numberOfDaysToSync: number;
+  startDate?: Date;
+  endDate?: Date;
+  debug: boolean;
+  ynabApiKey: string;
+  ynabBudgetId: string;
+  ynabAccountId: string;
+};
 
-  let startDate = subDays(new Date(), numberOfDaysToSync);
+export const exportTransactions = async (
+  params: WestpacTransactionExportParams
+) => {
+  let endDate = undefined;
 
-  let debug = false;
-
-  if (
-    (process.env["Sync.Westpac.Debug"] || "#{Sync.Westpac.Debug}" || "") ===
-    "true"
-  ) {
-    console.log("Running in debug mode");
-    debug = true;
+  if (params.endDate !== undefined) {
+    endDate = params.endDate;
   }
 
-  const inputs: WestpacTransactionExportInputs = {
-    username:
-      process.env["Sync.Westpac.Username"] || "#{Sync.Westpac.Username}" || "",
-    password:
-      process.env["Sync.Westpac.Password"] || "#{Sync.Westpac.Password}" || "",
-    accountName:
-      process.env["Sync.Westpac.AccountName"] ||
-      "#{Sync.Westpac.AccountName}" ||
-      "",
-    startDate: startDate,
-    debug: debug,
-  };
+  let startDate = subDays(endDate ?? new Date(), params.numberOfDaysToSync);
+
+  if (params.startDate !== undefined) {
+    startDate = params.startDate;
+  }
+
   const exporter = new WestpacTransactionExporter();
 
   console.log(
-    `Exporting westpac transactions with start date of '${format(
+    `Exporting westpac transactions with date range of '${format(
       startDate,
       "P"
-    )}'`
+    )}' to '${format(endDate ?? new Date(), "P")}'`
   );
 
-  const output = await exporter.export(inputs);
+  const output = await exporter.export({
+    username: params.westpacUsername,
+    password: params.westpacPassword,
+    accountName: params.westpacAccountName,
+    startDate: startDate,
+    endDate: endDate,
+    debug: params.debug,
+  });
 
   console.log(`Transactions exported successfully to '${output.filePath}'`);
 
   console.log(`Parsing transactions from '${output.filePath}'`);
 
   const parser: ITransactionParser = new OfxTransactionParser({
-    debug: debug,
+    debug: params.debug,
   });
 
-  const transactions = parser.parse(
-    process.env["Sync.Westpac.YnabAccountId"] ||
-      "#{Sync.Westpac.YnabAccountId}" ||
-      "",
-    output.filePath
-  );
+  const transactions = parser.parse(params.ynabAccountId, output.filePath);
 
   console.log(
     `Parsed '${transactions.length}' transactions from '${output.filePath}'`
@@ -73,33 +70,72 @@ const exportTransactions = async () => {
 
   const importer: ITransactionImporter = new YnabTransactionImporter({
     credentials: {
-      apiKey:
-        process.env["Sync.Westpac.YnabApiKey"] ||
-        "#{Sync.Westpac.YnabApiKey}" ||
-        "",
+      apiKey: params.ynabApiKey,
     },
-    debug: debug,
+    debug: params.debug,
   });
 
   console.log(`Importing '${transactions.length}' transactions into YNAB`);
 
-  await importer.import(
-    process.env["Sync.Westpac.YnabBudgetId"] ||
-      "#{Sync.Westpac.YnabBudgetId}" ||
-      "",
-    transactions
-  );
+  await importer.import(params.ynabBudgetId, transactions);
 
   console.log(
     `Imported '${transactions.length}' transactions into YNAB successfully`
   );
 };
 
-exportTransactions()
-  .then(() => {
-    process.exit(0);
-  })
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
+export const createWestpacAuSyncCommand = (): commander.Command => {
+  return new commander.Command("westpac-au")
+    .description("Sync Westpac Australia transactions to YNAB")
+    .requiredOption("--westpac-username <username>", "Westpac username")
+    .requiredOption("--westpac-password <password>", "Westpac password")
+    .requiredOption(
+      "--westpac-account-name  <account-name>",
+      "Name of Westpac account to sync from"
+    )
+    .option<number>(
+      "--number-of-days-to-sync <number-of-days-to-sync>",
+      "Numbers of days of transactions to sync",
+      (value: string) => parseInt(value),
+      7
+    )
+    .option<Date>(
+      "--start-date <start-date>",
+      "Start date to sync from in ISO format (yyyy-MM-dd). If this is set then --number-of-days-to-sync is ignored.",
+      (value: string) => {
+        const date = parseISO(value);
+        if (!isValid(date))
+          throw new commander.InvalidOptionArgumentError(
+            "Date must be be in format 'yyyy-MM-dd'"
+          );
+        return date;
+      },
+      undefined
+    )
+    .option<Date>(
+      "--end-date <end-date>",
+      "End date to sync to in ISO format (yyyy-MM-dd). If this is set then --number-of-days-to-sync is taken from this date.",
+      (value: string) => {
+        const date = parseISO(value);
+        if (!isValid(date))
+          throw new commander.InvalidOptionArgumentError(
+            "Date must be be in format 'yyyy-MM-dd'"
+          );
+        return date;
+      },
+      undefined
+    )
+    .requiredOption("--ynab-api-key <ynab-api-key>", "YNAB Api key")
+    .requiredOption(
+      "--ynab-budget-id <ynab-budget-id>",
+      "Id of YNAB budget to import into"
+    )
+    .requiredOption(
+      "--ynab-account-id <ynab-account-id>",
+      "Id of YNAB account to import into"
+    )
+    .option("--debug", "Whether to run in debug mode", false)
+    .action(async (args: WestpacTransactionExportParams) => {
+      await exportTransactions(args);
+    });
+};
